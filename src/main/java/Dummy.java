d790057a80f55250d677273cd5e08218263dac8b
LeonBrachwitz <mail@leon-brachwitz.de>
2020-10-12 20:46:52 +0200

    Build.gradle fuer Test anpassen und DummyTest hinzufuegen

---
+++ src/main/java/Dummy.java
@@ -0,0 +1,12 @@
+public class Dummy
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Main Methode");
+    }
+
+    public int dummyMethode(Integer wert1, Integer wert2)
+    {
+        return wert1 + wert2;
+    }
+}
---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DummyTest
+{
+    private Dummy sut = new Dummy();
+
+    @Test
+    @DisplayName("Dummy Sollte")
+    public void test01()
+    {
+        final var erwartet = Integer.valueOf(10);
+        final var ergebnis = sut.dummyMethode(5,5);
+
+        assertThat(erwartet).isEqualTo(ergebnis);
+    }
+}
